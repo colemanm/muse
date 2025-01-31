c9aca5a4836c8abc27362c18478e2f6688dfb158
Coleman McCormick <coleman@mcco.me>
2025-01-31 09:53:51 -0500

    undo

--- webpack.config.js
+++ webpack.config.js
@@ -5,31 +5,29 @@ const { DefinePlugin } = webpack;
 const path = require('path');
 const fs = require('fs');
 
-// Helper to safely load env file
-const loadEnvFile = (filepath) => {
-  try {
-    return require('dotenv').parse(fs.readFileSync(filepath));
-  } catch (err) {
-    // Return empty object if file doesn't exist
-    return {};
-  }
-};
-
 module.exports = async function (env, argv) {
-  const config = await createExpoWebpackConfigAsync(env, argv);
+  // Determine which env file to use
+  const envPath = process.env.NODE_ENV === 'production' 
+    ? path.resolve(__dirname, '.env.production')
+    : path.resolve(__dirname, '.env.development');
 
-  // Load env files, fallback to empty object if not found
-  const prodEnv = loadEnvFile(path.resolve(__dirname, '.env.production'));
-  const devEnv = loadEnvFile(path.resolve(__dirname, '.env.development'));
-  const defaultEnv = loadEnvFile(path.resolve(__dirname, '.env'));
+  // Read the appropriate .env file
+  const envVars = fs.readFileSync(envPath, 'utf8')
+    .split('\n')
+    .filter(line => line && !line.startsWith('#'))
+    .reduce((vars, line) => {
+      const [key, value] = line.split('=');
+      vars[key] = value.replace(/["']/g, '');
+      return vars;
+    }, {});
 
-  // Merge env variables, with Vercel env taking precedence
-  const envVariables = {
-    ...defaultEnv,
-    ...devEnv,
-    ...prodEnv,
-    ...process.env, // Vercel env variables take precedence
-  };
+  const config = await createExpoWebpackConfigAsync({
+    ...env,
+    template: './web/index.html',
+    babel: {
+      dangerouslyAddModulePathsToTranspile: ['@expo/webpack-config/web-default/index.html'],
+    },
+  }, argv);
 
   // Add rule for .md files
   config.module.rules.push({
@@ -57,12 +55,12 @@ module.exports = async function (env, argv) {
     new DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-        EXPO_PUBLIC_FIREBASE_API_KEY: JSON.stringify(envVariables.EXPO_PUBLIC_FIREBASE_API_KEY),
-        EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN: JSON.stringify(envVariables.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN),
-        EXPO_PUBLIC_FIREBASE_PROJECT_ID: JSON.stringify(envVariables.EXPO_PUBLIC_FIREBASE_PROJECT_ID),
-        EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET: JSON.stringify(envVariables.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET),
-        EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: JSON.stringify(envVariables.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID),
-        EXPO_PUBLIC_FIREBASE_APP_ID: JSON.stringify(envVariables.EXPO_PUBLIC_FIREBASE_APP_ID),
+        EXPO_PUBLIC_FIREBASE_API_KEY: JSON.stringify(envVars.EXPO_PUBLIC_FIREBASE_API_KEY),
+        EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN: JSON.stringify(envVars.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN),
+        EXPO_PUBLIC_FIREBASE_PROJECT_ID: JSON.stringify(envVars.EXPO_PUBLIC_FIREBASE_PROJECT_ID),
+        EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET: JSON.stringify(envVars.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET),
+        EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: JSON.stringify(envVars.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID),
+        EXPO_PUBLIC_FIREBASE_APP_ID: JSON.stringify(envVars.EXPO_PUBLIC_FIREBASE_APP_ID),
       },
       __DEV__: JSON.stringify(process.env.NODE_ENV !== 'production'),
     })
