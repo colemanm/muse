15dfb06385d412b7aa062c7afdd702eb42761539
Coleman McCormick <coleman@mcco.me>
2025-02-03 11:08:04 -0500

    Support Vercel deploy

--- webpack.config.js
+++ webpack.config.js
@@ -6,20 +6,35 @@ const path = require('path');
 const fs = require('fs');
 
 module.exports = async function (env, argv) {
-  // Determine which env file to use
-  const envPath = process.env.NODE_ENV === 'production' 
-    ? path.resolve(__dirname, '.env.production')
-    : path.resolve(__dirname, '.env.development');
-
-  // Read the appropriate .env file
-  const envVars = fs.readFileSync(envPath, 'utf8')
-    .split('\n')
-    .filter(line => line && !line.startsWith('#'))
-    .reduce((vars, line) => {
-      const [key, value] = line.split('=');
-      vars[key] = value.replace(/["']/g, '');
-      return vars;
-    }, {});
+  // Get environment variables either from .env files (local) or process.env (Vercel)
+  let envVars = {};
+  
+  try {
+    // Attempt to read from .env file
+    const envPath = process.env.NODE_ENV === 'production' 
+      ? path.resolve(__dirname, '.env.production')
+      : path.resolve(__dirname, '.env.development');
+    
+    const envFile = fs.readFileSync(envPath, 'utf8');
+    envVars = envFile
+      .split('\n')
+      .filter(line => line && !line.startsWith('#'))
+      .reduce((vars, line) => {
+        const [key, value] = line.split('=');
+        vars[key] = value.replace(/["']/g, '');
+        return vars;
+      }, {});
+  } catch (error) {
+    // If .env file doesn't exist, use process.env
+    envVars = {
+      EXPO_PUBLIC_FIREBASE_API_KEY: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+      EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      EXPO_PUBLIC_FIREBASE_PROJECT_ID: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+      EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+      EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+      EXPO_PUBLIC_FIREBASE_APP_ID: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+    };
+  }
 
   const config = await createExpoWebpackConfigAsync({
     ...env,
@@ -55,12 +70,12 @@ module.exports = async function (env, argv) {
     new DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-        EXPO_PUBLIC_FIREBASE_API_KEY: JSON.stringify(envVars.EXPO_PUBLIC_FIREBASE_API_KEY),
-        EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN: JSON.stringify(envVars.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN),
-        EXPO_PUBLIC_FIREBASE_PROJECT_ID: JSON.stringify(envVars.EXPO_PUBLIC_FIREBASE_PROJECT_ID),
-        EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET: JSON.stringify(envVars.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET),
-        EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: JSON.stringify(envVars.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID),
-        EXPO_PUBLIC_FIREBASE_APP_ID: JSON.stringify(envVars.EXPO_PUBLIC_FIREBASE_APP_ID),
+        EXPO_PUBLIC_FIREBASE_API_KEY: JSON.stringify(envVars.EXPO_PUBLIC_FIREBASE_API_KEY || process.env.EXPO_PUBLIC_FIREBASE_API_KEY),
+        EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN: JSON.stringify(envVars.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN || process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN),
+        EXPO_PUBLIC_FIREBASE_PROJECT_ID: JSON.stringify(envVars.EXPO_PUBLIC_FIREBASE_PROJECT_ID || process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID),
+        EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET: JSON.stringify(envVars.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET || process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET),
+        EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: JSON.stringify(envVars.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID),
+        EXPO_PUBLIC_FIREBASE_APP_ID: JSON.stringify(envVars.EXPO_PUBLIC_FIREBASE_APP_ID || process.env.EXPO_PUBLIC_FIREBASE_APP_ID),
       },
       __DEV__: JSON.stringify(process.env.NODE_ENV !== 'production'),
     })
